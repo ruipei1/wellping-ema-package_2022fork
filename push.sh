6209294ff831f0425358ce827096ea81daa84f47
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-10-26 21:13:56 -0400

    better push

--- push.sh
+++ push.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rm -r build/
+sleep 1
+
+rm -r dist/
+sleep 1
+
+rm -r wellping_ema_parser.egg-info
+sleep 1
+
 python3 setup.py sdist bdist_wheel
 sleep 3
-python3 -m twine upload --repository-url https://pypi.org/project/wellping-ema-parser/ dist/*
+python3 -m twine upload --verbose dist/*
